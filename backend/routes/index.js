6522328b0c6bdd2c8fc727ec85ca48530a768de1
jonathan Salguero <jsalg024@fiu.edu>
2021-05-20 14:26:00 -0400

    api routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,8 @@
 // backend/routes/index.js
 const express = require("express");
 const router = express.Router();
+const apiRouter = require("./api");
+router.use("/api", apiRouter);
 
 router.get("/hello/world", function (req, res) {
   console.log(req);
